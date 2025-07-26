89fa4d1cfcf6346b39623b6da45401e749b38b89
tanujbhatia24 <tanujbhatia0001@gmail.com>
2025-07-26 16:52:26 +0530

    Terraform Updates

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,10 @@ output "public_ip" {
 output "frontend_url" {
   value = "http://${aws_instance.ecommerce.public_ip}:3000"
 }
+
+output "backend_services_urls" {
+  value = "http://${aws_instance.ecommerce.public_ip}:3001/health"
+  value = "http://${aws_instance.ecommerce.public_ip}:3002/health"
+  value = "http://${aws_instance.ecommerce.public_ip}:3003/health"
+  value = "http://${aws_instance.ecommerce.public_ip}:3004/health"
+}
