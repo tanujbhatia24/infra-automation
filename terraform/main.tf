e2f9749ce6b41daa524360121ec88f715beaadb6
tanujbhatia24 <tanujbhatia0001@gmail.com>
2025-07-26 16:44:36 +0530

    Terraform Updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,6 +57,9 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  tags = {
+    Name = "ecommerce-sg"
+  }
 }
 
 resource "aws_instance" "ecommerce" {
