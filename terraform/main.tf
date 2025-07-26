b37e99eb3b9006a9db58e208d1fa44c98d838903
tanujbhatia24 <tanujbhatia0001@gmail.com>
2025-07-26 16:46:39 +0530

    Terraform Updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,7 +66,7 @@ resource "aws_instance" "ecommerce" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = aws_subnet.public.id
-  security_groups = [aws_security_group.ec2_sg.name]
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
   associate_public_ip_address = true
 
   user_data = <<-EOF
